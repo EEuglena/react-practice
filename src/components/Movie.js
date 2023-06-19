a0110133534338be2c0ab3ea5367ec89ec042da1
EEuglena <halfmoon51525@gmail.com>
2023-06-19 14:19:33 +0900

    React lecture progression

---
+++ src/components/Movie.js
@@ -0,0 +1,45 @@
+import PropTypes from "prop-types";
+import styles from "./Movie.module.css";
+import { Link } from "react-router-dom";
+
+function Movie({ id, title, summary, genres, coverImage }) {
+	return (
+		<div>
+			<hr />
+			<div className={styles.movie}>
+				<div>
+					<div>
+						<div className={styles.title}>
+							<Link to={`/movie/${id}`}>{title}</Link>
+						</div>
+					</div>
+					<div className={styles.summary}>
+						{summary.length > 500
+							? summary.substring(0, 500) + "..."
+							: summary}
+					</div>
+					<div>
+						{genres.map((genre, index) =>
+							index < 3 ? (
+								<span key={index} className={styles.genre}>
+									{genre}
+								</span>
+							) : null
+						)}
+					</div>
+				</div>
+				<img className={styles.cover} src={coverImage} alt={title} />
+			</div>
+		</div>
+	);
+}
+
+Movie.propTypes = {
+	id: PropTypes.number.isRequired,
+	title: PropTypes.string.isRequired,
+	summary: PropTypes.string.isRequired,
+	genres: PropTypes.arrayOf(PropTypes.string).isRequired,
+	coverImage: PropTypes.string.isRequired,
+};
+
+export default Movie;
---
+++ src/routes/Home.js
@@ -0,0 +1,45 @@
+import { useState, useEffect } from "react";
+import styles from "./Home.module.css";
+import Movie from "../components/Movie";
+
+function Home() {
+	const [loading, setLoading] = useState(true);
+	const [movies, setMovies] = useState([]);
+
+	const getMovies = async () => {
+		const response = await fetch(
+			"https://yts.mx/api/v2/list_movies.json?minimum_rating=9&sort_by=year"
+		);
+		const json = await response.json();
+		setMovies(json.data.movies);
+		setLoading(false);
+	};
+
+	useEffect(() => {
+		getMovies();
+	}, []);
+
+	return (
+		<div className={styles.main}>
+			<h1 className={styles.title}>Movie Archive</h1>
+			{loading ? (
+				<strong>Loading...</strong>
+			) : (
+				<div>
+					{movies.map((movie) => (
+						<Movie
+							key={movie.id}
+							id={movie.id}
+							title={movie.title}
+							summary={movie.summary}
+							genres={movie.genres}
+							coverImage={movie.medium_cover_image}
+						/>
+					))}
+				</div>
+			)}
+		</div>
+	);
+}
+
+export default Home;
