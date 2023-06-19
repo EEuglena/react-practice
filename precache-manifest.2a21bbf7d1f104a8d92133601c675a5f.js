253f681316910fcfb37927972c822bdf5e190a91
EEuglena <halfmoon51525@gmail.com>
2023-06-19 15:34:55 +0900

    Updates

---
+++ precache-manifest.2a21bbf7d1f104a8d92133601c675a5f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "47e31557b90ea4aff08d",
+    "url": "/react-practice/static/css/main.dc4d4211.chunk.css"
+  },
+  {
+    "revision": "47e31557b90ea4aff08d",
+    "url": "/react-practice/static/js/main.47e31557.chunk.js"
+  },
+  {
+    "revision": "4dfb30c92d1ec79f19a8",
+    "url": "/react-practice/static/js/1.4dfb30c9.chunk.js"
+  },
+  {
+    "revision": "f878aa0a124d9b346b05",
+    "url": "/react-practice/static/js/runtime~main.f878aa0a.js"
+  },
+  {
+    "revision": "e23653d845f66a43c47b8ab9681f86e2",
+    "url": "/react-practice/index.html"
+  }
+];
